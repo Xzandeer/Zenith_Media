c4a8dd01a30fc85687d75cdb9fb488e8d9d12233
kasheeemsiborboro-lang <kasheeem.siborboro@gmail.com>
2025-12-07 23:49:45 +0800

    Update page.tsx

--- app/blogs/blog4/page.tsx
+++ app/blogs/blog4/page.tsx
@@ -37,7 +37,7 @@ export default function ThinkBeforeYouClick() {
         {/* Header Image - TEMP placeholder (you can update file) */}
         <div className="w-full h-[380px] md:h-[460px] mb-12 overflow-hidden rounded-lg animate-fadeInSlow">
           <img
-            src="/dawn.png"
+            src="/dawn.jpg"
             alt="Think Before You Click Thumbnail"
             className="w-full h-full object-cover"
           />
@@ -205,4 +205,5 @@ export default function ThinkBeforeYouClick() {
     </main>
   );
 }
+
 
