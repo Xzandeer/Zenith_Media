97fb851c3d4e336ecfaef411b8aeffc2d0fe6032
Xzandeer <schoolaccxander@gmail.com>
2025-12-07 21:20:20 +0800

    Update page.tsx

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -47,7 +47,7 @@ export default function BlogsPage() {
             </div>
           </a>
 
-          {/* ---- BLOG 2 ---- */}
+          {/* ---- NEW BLOG: NOT POSTING ---- */}
           <a
             href="/blogs/notposting"
             className="rounded-xl overflow-hidden shadow-lg bg-white hover:scale-[1.02] transition-all duration-300"
@@ -64,7 +64,7 @@ export default function BlogsPage() {
             </div>
           </a>
 
-          {/* ---- BLOG 3 Placeholder ---- */}
+          {/* ---- BLOG 2 Placeholder ---- */}
           <div className="rounded-xl overflow-hidden shadow-lg bg-white">
             <div className="w-full h-56 bg-gray-300"></div>
             <div className="bg-[#C25A5A] p-5 text-white">
@@ -72,102 +72,6 @@ export default function BlogsPage() {
               <p className="text-xs mt-2">More posts will be added soon.</p>
             </div>
           </div>
-
-          {/* ---- BLOG 4 ---- */}
-          <a
-            href="/blogs/blog4"
-            className="rounded-xl overflow-hidden shadow-lg bg-white hover:scale-[1.02] transition-all duration-300"
-          >
-            <img src="/blog4.jpg" className="w-full h-56 object-cover" />
-            <div className="bg-[#E6C7A3] p-5 text-[#6A4B3C]">
-              <h2 className="text-lg font-semibold">Blog 4 Title</h2>
-              <p className="text-xs mt-2">By Author Name <br /> Date</p>
-            </div>
-          </a>
-
-          {/* ---- BLOG 5 ---- */}
-          <a
-            href="/blogs/blog5"
-            className="rounded-xl overflow-hidden shadow-lg bg-white hover:scale-[1.02] transition-all duration-300"
-          >
-            <img src="/blog5.jpg" className="w-full h-56 object-cover" />
-            <div className="bg-[#D9A679] p-5 text-[#6A4B3C]">
-              <h2 className="text-lg font-semibold">Blog 5 Title</h2>
-              <p className="text-xs mt-2">By Author Name <br /> Date</p>
-            </div>
-          </a>
-
-          {/* ---- BLOG 6 ---- */}
-          <a
-            href="/blogs/blog6"
-            className="rounded-xl overflow-hidden shadow-lg bg-white hover:scale-[1.02] transition-all duration-300"
-          >
-            <img src="/blog6.jpg" className="w-full h-56 object-cover" />
-            <div className="bg-[#B76E54] p-5 text-white">
-              <h2 className="text-lg font-semibold">Blog 6 Title</h2>
-              <p className="text-xs mt-2">By Author Name <br /> Date</p>
-            </div>
-          </a>
-
-          {/* ---- BLOG 7 ---- */}
-          <a
-            href="/blogs/blog7"
-            className="rounded-xl overflow-hidden shadow-lg bg-white hover:scale-[1.02] transition-all duration-300"
-          >
-            <img src="/blog7.jpg" className="w-full h-56 object-cover" />
-            <div className="bg-[#CE9B64] p-5 text-[#6A4B3C]">
-              <h2 className="text-lg font-semibold">Blog 7 Title</h2>
-              <p className="text-xs mt-2">By Author Name <br /> Date</p>
-            </div>
-          </a>
-
-          {/* ---- BLOG 8 ---- */}
-          <a
-            href="/blogs/blog8"
-            className="rounded-xl overflow-hidden shadow-lg bg-white hover:scale-[1.02] transition-all duration-300"
-          >
-            <img src="/blog8.jpg" className="w-full h-56 object-cover" />
-            <div className="bg-[#CA8B57] p-5 text-white">
-              <h2 className="text-lg font-semibold">Blog 8 Title</h2>
-              <p className="text-xs mt-2">By Author Name <br /> Date</p>
-            </div>
-          </a>
-
-          {/* ---- BLOG 9 ---- */}
-          <a
-            href="/blogs/blog9"
-            className="rounded-xl overflow-hidden shadow-lg bg-white hover:scale-[1.02] transition-all duration-300"
-          >
-            <img src="/blog9.jpg" className="w-full h-56 object-cover" />
-            <div className="bg-[#B4683A] p-5 text-white">
-              <h2 className="text-lg font-semibold">Blog 9 Title</h2>
-              <p className="text-xs mt-2">By Author Name <br /> Date</p>
-            </div>
-          </a>
-
-          {/* ---- BLOG 10 ---- */}
-          <a
-            href="/blogs/blog10"
-            className="rounded-xl overflow-hidden shadow-lg bg-white hover:scale-[1.02] transition-all duration-300"
-          >
-            <img src="/blog10.jpg" className="w-full h-56 object-cover" />
-            <div className="bg-[#E1B07C] p-5 text-[#6A4B3C]">
-              <h2 className="text-lg font-semibold">Blog 10 Title</h2>
-              <p className="text-xs mt-2">By Author Name <br /> Date</p>
-            </div>
-          </a>
-
-          {/* ---- BLOG 11 ---- */}
-          <a
-            href="/blogs/blog11"
-            className="rounded-xl overflow-hidden shadow-lg bg-white hover:scale-[1.02] transition-all duration-300"
-          >
-            <img src="/blog11.jpg" className="w-full h-56 object-cover" />
-            <div className="bg-[#D6A168] p-5 text-[#6A4B3C]">
-              <h2 className="text-lg font-semibold">Blog 11 Title</h2>
-              <p className="text-xs mt-2">By Author Name <br /> Date</p>
-            </div>
-          </a>
 
         </div>
       </div>
